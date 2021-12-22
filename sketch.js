0d882858325cd7141fb05f30e35f68aff2dcb449
Samuel Perroud <sam@supero.ch>
2021-12-22 16:58:01 +0100

    update

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup() {
   textFont(myFont)
   textSize(240)
   textAlign(CENTER)
-  //createLoop({duration:10, gif:true})
+  createLoop({duration:10, gif:true})
 }
 
 let rotationPos = 0
@@ -24,13 +24,11 @@ let a = 0
 let floconSize = 50
 let floconMaxSize = 90
 let vitesseRotation = 0.01
-let vitesseZoom = 1.5
+let vitesseZoom = 2.5
 
 function draw() {
-  background(37,60,120)
-  fill(236, 103, 15)
-  text('JOYEUSES', canvasWidth/2, canvasHeight/2-30);
-  text('FÊTES!', canvasWidth/2, canvasHeight/2+170);
+  background(0,0,100)
+
 
   let numRows = 6
   let numbCols = 12
@@ -41,15 +39,11 @@ function draw() {
 
   for (let row=0; row < numRows; row+=1){
     for (let col=0; col < numbCols ; col+=1){
-      console.log(cos(a))
+      console.log(cos(sin(a + row + col)))
       push()
       floconSize = sin(a + row + col) * floconMaxSize
       translate(col*cellSizeWidth + cellSizeWidth/2, row*cellSizeHeight + cellSizeHeight/2)
-      if(col%2 == 0){
-        rotate(rotationPos)
-      } else{
-        rotate(rotationNeg)
-      }
+      rotate(sin(a + row * col))
       image(flocon, -floconSize/2, -floconSize/2, floconSize, floconSize)
       pop()
     }
@@ -58,7 +52,7 @@ function draw() {
   rotationPos +=vitesseRotation
   rotationNeg -=vitesseRotation
 
-  fill(236, 103, 15, 150)
+  fill(236, 103, 15)
   text('JOYEUSES', canvasWidth/2, canvasHeight/2-30);
   text('FÊTES!', canvasWidth/2, canvasHeight/2+170);
 
